0076b3fbcae916e96039566c5e57895a3ee77710
enoamaore14120 <121623851+enoamaore14120@users.noreply.github.com>
2025-02-13 15:09:04 +0100

    FishEye avec modal presque ok

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,16 @@
 function displayModal() {
+    // 1. Récupérer la modale
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    
+    // 2. Récupérer le texte du nom du photographe (l’élément h1.artist-name)
+    const artistNameElement = document.querySelector(".artist-name");
+    if (artistNameElement) {
+      // 3. Copier ce texte dans la div #artiste-name-modal
+      document.getElementById("modalNameDiv").textContent = artistNameElement.textContent;
+    }
+
+    // 4. Afficher la modale
+    modal.style.display = "block";
 }
 
 function closeModal() {
